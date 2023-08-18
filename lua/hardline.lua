4e200d6ec2a80f584dfd40b0896bd0537101e0ce
jasonjurotich <jjir.org>
2023-08-18 11:53:49 -0600

    updated

--- lua/hardline.lua
+++ lua/hardline.lua
@@ -4,7 +4,6 @@
 
 -------------------- VARIABLES -----------------------------
 local common = require('hardline.common')
-local custom_colors = require('hardline.themes.custom_colors')
 local fmt = string.format
 local M = {}
 
@@ -20,19 +19,6 @@ M.options = {
     {class = 'error', item = require('hardline.parts.lsp').get_error},
     {class = 'warning', item = require('hardline.parts.lsp').get_warning},
     {class = 'warning', item = require('hardline.parts.whitespace').get_item},
-  },
-  custom_theme = {
-    text = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    normal = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    insert = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    replace = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    inactive_comment = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    inactive_cursor = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    inactive_menu = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    visual = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    command = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    alt_text = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
-    warning = {gui = "NONE", cterm = "NONE", cterm16 = "NONE"},
   },
 }
 
@@ -140,15 +126,8 @@ end
 
 -------------------- SETUP -----------------------------
 local function set_theme()
-  if type(M.options.theme) ~= 'string' then
-    return
-  end
-  if M.options.theme == 'custom' then
-    M.options.theme = custom_colors.set(M.options.custom_theme)
-  else
-    local theme = fmt('hardline.themes.%s', M.options.theme)
-    M.options.theme = require(theme)
-  end
+  local theme = fmt('hardline.themes.%s', M.options.theme)
+  M.options.theme = require(theme)
 end
 
 local function set_hlgroups()
